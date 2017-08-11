0e267e2f445aa4d50e072222eb55e44e3ea99e75
Deepesh Pathak <deepshpathak@gmail.com>
2017-08-11 06:52:08 +0530

    update script to handle keyboard events in form handling

--- Frontend/form-page/script.js
+++ Frontend/form-page/script.js
@@ -1,6 +1,6 @@
 window.onload = function() {
     const WIDTH = 44
-    let nextInput = function(e) {
+    let nextInput = function() {
         let formBody = document.getElementsByClassName("form-body")[0];
         let maxOffset = document.getElementsByClassName("input-element").length * 44;
         let newOffset = parseInt(formBody.style.left? formBody.style.left: 0) - WIDTH;
@@ -18,10 +18,32 @@ window.onload = function() {
         alert("Nothing before this");
     }
 
-    let previousBtn = document.getElementsByClassName("previous");
-    for(let i = 0; i < previousBtn.length; i++)
-        previousBtn[i].addEventListener('click', previousInput)
-    let nextBtn = document.getElementsByClassName("next");
-    for(let i = 0; i < nextBtn.length; i++)
-        nextBtn[i].addEventListener('click', nextInput)
+    let listenKeydown = function(e) {
+        switch (e.keyCode) {
+            case 39:
+                nextInput()
+                break;
+            case 37:
+                nextInput()
+                break;
+            case 9:
+                e.preventDefault();
+                nextInput();
+                break;
+            case 13:
+                e.preventDefault();
+                document.getElementsByClassName("form-body")[0].submit();
+                break;
+            default:
+
+        }
+        console.log(e);
+    }
+
+    let previousBtn = document.getElementsByClassName("previous")[0];
+    let nextBtn = document.getElementsByClassName("next")[0];
+
+    previousBtn.addEventListener('click', previousInput)
+    nextBtn.addEventListener('click', nextInput)
+    document.addEventListener("keydown", listenKeydown);
 }
