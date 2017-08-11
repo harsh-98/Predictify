10abd9dfbec123efa54f9434a787d2682ce6151b
ankitkataria <ankitkataria28@gmail.com>
2017-08-11 21:36:11 +0530

    Added form page for user information

--- andha_predictor/dashboard/views.py
+++ andha_predictor/dashboard/views.py
@@ -7,7 +7,7 @@ from .predictor import billu_scorer
 
 def index(request):
 	if request.user.is_authenticated:
-		return render(request,'dashboard/index.html',{'line':[7,8,9],'pie1':[  2.87356322,   6.03448276,  63.2183908 ,  25.86206897,   2.01149425],'pie2':[  3.35249042,   8.33333333,  66.2835249 ,  16.76245211,   5.26819923],'average': 14})
+		return render(request,'dashboard/form.html',{})
 	else: 
 		return HttpResponse("ERROR 404")
 
@@ -16,9 +16,42 @@ def logoutUser(request):
 	return redirect('/login/')
 
 def predictScore(request):
-	data = ['F',15,'U','GT3','T',4,2,'health','services','home','mother',1,3,0,'no','yes','yes','yes','yes','yes','yes','yes',3,2,2,1,1,5,2,15,14]
+	# data = ['F',15,'U','GT3','T',4,2,'health','services','home','mother',1,3,0,'no','yes','yes','yes','yes','yes','yes','yes',3,2,2,1,1,5,2,15,14]
+	data = []
+	data.append(request.POST['sex'])
+	data.append(request.POST['age'])
+	data.append(request.POST['address'])
+	data.append(request.POST['famsize'])
+	data.append(request.POST['Pstatus'])
+	data.append(request.POST['Medu'])
+	data.append(request.POST['Fedu'])
+	data.append(request.POST['Mjob'])
+	data.append(request.POST['Fjob'])
+	data.append(request.POST['reason'])
+	data.append(request.POST['guardian'])
+	data.append(request.POST['traveltime'])
+	data.append(request.POST['studytime'])
+	data.append(request.POST['failures'])
+	data.append(request.POST['schoolsup'])
+	data.append(request.POST['famsup'])
+	data.append(request.POST['paid'])
+	data.append(request.POST['activities'])
+	data.append(request.POST['nursery'])
+	data.append(request.POST['higher'])
+	data.append(request.POST['internet'])
+	data.append(request.POST['romantic'])
+	data.append(request.POST['famrel'])
+	data.append(request.POST['freetime'])
+	data.append(request.POST['goout'])
+	data.append(request.POST['Dalc'])
+	data.append(request.POST['Walc'])
+	data.append(request.POST['health'])
+	data.append(request.POST['absences'])
+	data.append(request.POST['G1'])
+	data.append(request.POST['G2'])
+
 	graph_data = billu_scorer(data)
-	print(type(graph_data['pie_charts'][0]))
+	# print(type(graph_data['pie_charts'][0]))
 	return render(request,'dashboard/index.html',{'line':graph_data['scores'],'pie1':graph_data['pie_charts'][0],'pie2':graph_data['pie_charts'][1],'average': graph_data['average'][0] })
 	# return HttpResponse("done")
 
